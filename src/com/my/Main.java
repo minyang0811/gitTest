cfd1cae1837bad190fcc5f880aed3c9d1c2b00c1
my <MY1997811@163.com>
2022-08-06 17:11:02 +0800

    master 不管了

--- src/com/my/Main.java
+++ src/com/my/Main.java
@@ -2,11 +2,6 @@ package com.my;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello master2");
-        System.out.println("no gg");
-        System.out.println("gg2");
-        System.out.println("gg333");
-        System.out.println("为什么会分支");
+        System.out.println("hello master");
     }
 }
