42d23ddfa93f3be3c43bfea1f5cf7a9501f814a9
my <MY1997811@163.com>
2022-08-06 16:25:14 +0800

    gg 第2次提交

--- src/com/my/Main.java
+++ src/com/my/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello master2");
-        System.out.println("gg");
+        System.out.println("gg333");
     }
 }
